e495a1e2c390a41b13f05f4e2f7f4aaa05198ce3
Davidaprilio <david1.aprilio4@gmail.com>
2022-09-10 17:32:19 +0700

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+# dicoding BackEnd notes-app-back-end
+
+project ini adalah latihan membuat rest api dari course dicoding.com
+
+materi praktik:
+membuat app notes yang dibangun dengan Hapi Freamwork, menggunakan Joi untuk validator-nya dan menerapkan custom error.
+
---
+++ src/server.js
@@ -0,0 +1,34 @@
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+// const routes = require('./routes');
+
+const init = async () => {
+    const notesService = new NotesService();
+
+    const server = Hapi.server({
+        port: 5000,
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    // server.route(routes);
+
+    await server.register({
+        plugin: notes,
+        options: {
+            service: notesService,
+            validator: NotesValidator,
+        },
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
