149f1370754dc4d229d9cdf9118498069ce1f2f8
Davidaprilio <david1.aprilio4@gmail.com>
2022-09-12 16:12:48 +0700

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -70,7 +70,7 @@ class NotesHandler {
             const { id } = request.params;
             const { id: credentialId } = request.auth.credentials;
 
-            await this._service.verifyNoteOwner(id, credentialId);
+            await this._service.verifyNoteAccess(id, credentialId);
             const note = await this._service.getNoteById(id);
 
             return {
@@ -106,7 +106,7 @@ class NotesHandler {
             const { id } = request.params;
             const { id: credentialId } = request.auth.credentials;
 
-            await this._service.verifyNoteOwner(id, credentialId);
+            await this._service.verifyNoteAccess(id, credentialId);
             await this._service.editNoteById(id, request.payload);
 
             return {
